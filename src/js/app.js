e107e1f93dbfa14cafebd07a114bd9bf9f5deeec
Skullage <dogonadze1999@gmail.com>
2022-08-22 01:54:12 +0500

    Delete src directory

--- src/js/app.js
+++
@@ -1,8 +0,0 @@
-import * as Functions from "./modules/functions.js";
-import * as Swiper from "./modules/swiper.js";
-
-Functions.isWebp();
-Functions.Timer();
-Functions.checkNumber();
-Functions.scroll();
-Swiper.swiper;
